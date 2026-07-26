1eeae44a73e97ead6e0c9eabc35ef3652743c160
ga2495 <guptaaman88732@gmail.com>
2026-07-26 19:04:31 +0530

    Fix Vercel CORS

--- backend/api/main.py
+++ backend/api/main.py
@@ -12,6 +12,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://honeywell-ai-cybersecurity-dashboard.vercel.app",
+        "https://honeywell-ai-cybersecurity-dashboard-bntjw32n2.vercel.app",
+        "https://honeywell-ai-cybersecurity-da-git-77a112-guptaaman88732gmailcom.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
